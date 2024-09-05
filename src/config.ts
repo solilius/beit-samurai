72b295aec848b29440408886840e7f3586ee45ff
Sol Elan <solipod8@gmail.com>
2024-09-05 20:17:08 +0900

    Replaces the client to React

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables from .env file
 
 export const config = {
+    port: Number(process.env.PORT || 3000),
     spreadsheetId: String(process.env.SPREADSHEET_ID || ''),
     maxBedCapacity: Number(process.env.MAX_BED_CAPACITY || 0),
     serviceAccountEmail: String(process.env.SERVICE_ACCOUNT_EMAIL || ''),
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 import { getAvailableBeds } from './flows/get-available-beds';
+import { config } from './config';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = config.port;
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, '../../client/build')));
 
 app.get('/available-beds', async (req: Request, res: Response) => {
   const months = (req.query.months as string).split(',');
   const bookings = await getAvailableBeds(months);
   res.json(bookings);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
