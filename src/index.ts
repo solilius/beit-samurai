4a1146b156beebb05d9733580442efe9321163d1
Sol Elan <solipod8@gmail.com>
2024-09-05 19:28:23 +0900

    Host website

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
 import { getAvailableBeds } from './flows/get-available-beds';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
 
 app.get('/available-beds', async (req: Request, res: Response) => {
   const months = (req.query.months as string).split(',');
